2a48b4ac5b056f77ad4f56ca706214b9850ae51c
daniel <g.thirteen333@gmail.com>
2019-07-02 21:44:33 -0400

    tree structure project

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,22 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+    if (preorder.length < 1 && inorder.length < 1) return null;
+    let rootVal = preorder[0];
+    let root = new TreeNode(rootVal);
 
+    let midIdx = inorder.indexOf(rootVal);
+    let leftinorder = inorder.slice(0, midIdx);
+    let rightinorder = inorder.slice(midIdx + 1);
+    
+    let leftPreorder = preorder.filter((val) => leftinorder.includes(val));
+    let rightPreorder = preorder.filter((val) => rightinorder.includes(val))
+
+    let leftTree = buildTree(leftPreorder, leftinorder);
+    let rightTree = buildTree(rightPreorder, rightinorder)
+    
+    root.left = leftTree;
+    root.right = rightTree;
+
+    return root
 }
