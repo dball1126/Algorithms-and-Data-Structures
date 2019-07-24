18c5234608b91e4969369d2116d704683843dd5a
daniel <g.thirteen333@gmail.com>
2019-07-24 15:20:41 -0400

    depth and breadth first search complete

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,14 @@
 function breadthFirstArray(root) {
-    
+    if(!root) return [];
+    let queue = [root];
+    let visited = [];
+    while (queue.length){
+        let node = queue.shift();
+        visited.push(node.val);
+        if (node.left) queue.push(node.left)
+        if (node.right) queue.push(node.right)
+    }
+    return visited;
 }
 
 module.exports = {
