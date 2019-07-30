c40ec6821135db4987bc20c0a760a3caf56e05bd
daniel <g.thirteen333@gmail.com>
2019-07-30 00:30:00 -0400

    queue project complete

--- queue_project/lib/queue.js
+++ queue_project/lib/queue.js
@@ -22,49 +22,48 @@
 
 class Node {
     constructor(val){
-        this.value = val;
-        this.next = null;
+       this.value = val;
+       this.next = null;
     }
 }
 
 class Queue {
     constructor(){
-        this.front = null;
-        this.back = null;
-        this.length = 0;
+       this.front = null;
+       this.back = null;
+       this.length = 0;
+      
     }
 
     enqueue(val){
-        const newNode = new Node(val);
-    if (!this.front) {
-        this.front = newNode;
-        this.back = newNode;
-    } else {
-        let temp = this.back;
-        this.back = newNode
-        temp.next = this.back;
-    }
-    this.length++;
-    return this.length;
+        let node = new Node(val);
+        if (!this.front) {
+            this.front = node;
+            this.back = node;
+        } else {
+            this.back.next = node;
+            this.back = node;
+        }
+        this.length++;
+        return this.size();
 
     }
 
     dequeue(){
-        if(!this.front) return null;
-        let node = this.front;
-
-        if(this.length === 1){
-            this.front = null;
-            this.back = null;
-        } else {
-            let temp = this.front.next;
-            this.front = temp;
-        }
-        this.length--;
-        return node.value;
+      if (!this.front) return null;
+      let node = this.front;
+      this.length--;
+      if (this.length === 0) {
+          this.front = null;
+          this.back = null;
+          return node.value;
+      } else {
+          this.front = this.front.next;
+          return node.value;
+      }
     }
     size(){
-        return this.length;
+       return this.length;
     }
 }
 
