85092c2e153121d47437eb9a8b794767f3d90ac3
daniel <g.thirteen333@gmail.com>
2019-07-31 14:04:14 -0400

    tries project update

--- trie_project/lib/trie.js
+++ trie_project/lib/trie.js
@@ -65,6 +65,32 @@ class Trie {
 
        return node.isTerminal
    }
+   wordsWithPrefix(prefix, root=this.root) {
+       if (prefix.length === 0) {
+       let allWords = [];
+       if (root.isTerminal) allWords.push('');
+
+       for (let letter in root.children) {
+           let child = root.children[letter];
+           let suffixes = this.wordsWithPrefix(prefix, child);
+           let words = suffixes.map(suffix => letter + suffix)
+           allWords.push(...words);
+       }
+
+       return allWords;
+    } else {
+        let firstLetter = prefix[0];
+        let child = root.children[firstLetter];
+        if (child === undefined) {
+            return [];
+        } else {
+
+            let suffixes = this.wordsWithPrefix(prefix.slice(1), root.children[firstLetter]);
+            let words = suffixes.map(suffix => firstLetter + suffix);
+            return words;
+        }
+    }
+   }
 } 
 
 module.exports = {
