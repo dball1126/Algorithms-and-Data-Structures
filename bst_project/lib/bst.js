1bb1c750592b404e88c0970b60adb14092f605e2
daniel <g.thirteen333@gmail.com>
2019-08-02 11:23:04 -0400

    linked list cycle project update

--- bst_project/lib/bst.js
+++ bst_project/lib/bst.js
@@ -1,58 +1,55 @@
 class TreeNode {
     constructor(val) {
-      this.val = val;
-      this.left = null;
-      this.right = null;
+       this.val = val;
+       this.left = null;
+       this.right = null;
     }
 }
 
 
 class BST {
    constructor(){
-      this.root = null
+       this.root = null;
    }
    insert(val, root = this.root){
-        if(!this.root) {
-            this.root = new TreeNode(val);
-            return;
+        let node = new TreeNode(val);
+        if (!root) return this.root = node;
+        if (root.val > val) {
+            if (!root.left) {
+                root.left = node;
+            } else {
+                this.insert(val, root.left)
+            }
+        } else {
+            if (!root.right) {
+                root.right = node;
+            } else {
+                this.insert(val, root.right)
+            }
         }
-       if (val < root.val) {
-           if (!root.left) {
-               root.left = new TreeNode(val);
-           } else {
-               this.insert(val, root.left)
-           }
-       } else {
-           if (!root.right) {
-               root.right = new TreeNode(val);
-           } else {
-               this.insert(val, root.right);
-           }
-       }
+        return this;
     }
 
-    searchRecur(val, root=this.root){
+    searchRecur(val, root = this.root){
         if (!root) return false;
-
-        if ( val < root.val) {
+        if (root.val === val) return true;
+        if (root.val > val) {
             return this.searchRecur(val, root.left);
-        } else if (val > root.val){
-            return this.searchRecur(val, root.right);
         } else {
-            return true;
+            return this.searchRecur(val, root.right);
         }
+       
     }
 
     searchIter(val) {
-        let curr = this.root;
-
-        while (curr) {
-            if (val < curr.val) {
-                curr = curr.left;
-            } else if (val > curr.val) {
-                curr = curr.right;
+        if (!this.root) return false;
+        let current = this.root;
+        while (current) {
+            if (current.val === val) return true;
+            if (current.val > val) {
+                current = current.left;
             } else {
-                return true;
+                current = current.right;
             }
         }
         return false;
