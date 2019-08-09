16cc6eba9b5c3d3bf3cd51dc7ad86bd54c892098
daniel <g.thirteen333@gmail.com>
2019-08-09 02:46:12 -0400

    third test added to  moveToFront for doubly linked list

--- linked_list_interview_problems/test/test.js
+++ linked_list_interview_problems/test/test.js
@@ -645,23 +645,30 @@ describe('Problem 5: Doubly Linked List', () => {
                     list.moveToFront(node)
                     expect(list.head).to.equal(node = list.tail)
                 })
+                
                 it('Should rearrange the head pointer with multiple nodes in list and not the head', () => {
                     list.push(node)
+                    list.push('D')
                     list.moveToFront(node)
                     expect(node).to.equal(list.head)
                 })
-                // it('Should rearrange the next head pointer with multiple nodes in list and not the head', () => {
-                //     list.push(node)
 
-                //     list.push('D');
-                //     list.moveToFront(node)
-                //     expect(node.next).to.equal(list.tail)
-                // })
-                // it('Should rearrange the prev head pointer with multiple nodes in list and not the head', () => {
-                //     list.push(node)
-                //     list.moveToFront(node)
-                //     expect(node.prev).to.equal(list.head.prev)
-                // })
+                it('Should rearrange the next tail pointer with multiple nodes in list', () => {
+                    list.push(node)
+                    list.push('D');
+                    nodeTest = list.head
+                    list.moveToFront(node)
+                    expect(list.head.next).to.equal(nodeTest)
+                })
+  // THIS METHOD NEEDS TO BE TESTED //  
+                it('Should rearrange the prev head pointer with multiple nodes in list', () => {
+                    list.push(node)
+                    list.push('D');
+                    nodeTest = list.tail
+                    list.moveToFront(node)
+                    expect(list.head.prev).to.equal(null)
+                })
+                
             });
 
             describe('moveToEnd', () => {
