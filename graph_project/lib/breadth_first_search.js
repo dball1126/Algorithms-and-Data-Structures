e221ecf25b02bc71e8444fe014457529b12c355d
daniel <g.thirteen333@gmail.com>
2019-08-22 21:20:42 -0400

    update to graph project for breadth first search

--- graph_project/lib/breadth_first_search.js
+++ graph_project/lib/breadth_first_search.js
@@ -1,14 +1,21 @@
 function breadthFirstSearch(startingNode, targetVal) {
-   let queue = [ startingNode ];
-   let newSet = new Set();
-   while(queue.length) {
-       node = queue.shift();
-       if (newSet.has(node)) continue;
-       newSet.add(node);
-       if (node.val === targetVal) return node;
-        queue.push(...node.neighbors)
-   }
-   return null;
+    let queue = [ startingNode ];
+    let visited = new Set();
+    while (queue.length) {
+        let ele = queue.shift();
+        visited.add(ele.val)
+       if (ele.val === targetVal) return ele;
+      if (ele.neighbors) {
+          ele.neighbors.forEach(e => {
+              if (!visited.has(e.val)) {
+              queue.push(e)
+              }
+          })
+      } else {
+          return null;
+      }
+    }
+    return null;
 }
 
 module.exports = {
