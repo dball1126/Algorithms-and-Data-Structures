ac7474d49174d9241b06af0b45f5e16b2cc10762
daniel <g.thirteen333@gmail.com>
2019-09-13 15:39:19 -0400

    graph project update

--- graph_project/lib/num_regions.js
+++ graph_project/lib/num_regions.js
@@ -1,12 +1,34 @@
 function numRegions(graph) {
-   
+    let queue = [graph];
+
+    let count = 0;
+    let visited = new Set();
+    let node = queue.shift();
+    // let boolean = [];
+    for (let i in node) {
+        let stack = [i];
+        let boolean = [];
+
+
+        if (!visited.has(i)) count++;
+
+        while (stack.length) {
+            let inner = stack.pop();
+
+            if (!visited.has(inner)) {
+                visited.add(inner)
+
+                for (let i in graph[inner]) {
+                    stack.push(graph[inner][i])
+                }
+            }
+
+        }
+
+
+    }
+    return count;
 }
-
-function isNewRegion(node, graph, visited) {
-    
-}
-
-
 module.exports = {
     numRegions
 };
