e10c28a142bd4335047ed2f07f7ab870156895f2
daniel <g.thirteen333@gmail.com>
2020-01-07 17:59:12 -0500

    insertion sort function complete

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,17 +1,13 @@
 function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let currentEle = arr[i];
-        for (let j = i-1; j >= 0 && currentEle < arr[j]; j--) {
-            element2 = arr[j+1];
-            element2 = arr[j];
-            console.log(element2)
-        }
-        console.log(arr)
-        element2 = currentEle;
-    }
-    return arr;
+ for (let i = 1; i < arr.length; i++) {
+     const current = arr[i];
+     for(j = i - 1; j >= 0 && current < arr[j]; j--) {
+         arr[j + 1] = arr[j]
+     }
+     arr[j + 1] = current
+ }
+ return arr
 }
-console.log(insertionSort([2,1,3,-2]))
 
 module.exports = {
     insertionSort
