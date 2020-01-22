dcd463a7854434a8cecc31ea883e924d271a747f
daniel <g.thirteen333@gmail.com>
2020-01-22 17:05:52 -0500

    left right rotation function complete

--- bst_project/lib/rotations.js
+++ bst_project/lib/rotations.js
@@ -45,13 +45,37 @@ function leftRightRotation(node) {
         
         changeNode.parent = root;
         changeNode.left = pivot;
-        changeNode.side = left;
+        changeNode.side = 'left';
 
         pivot.parent = changeNode;
         pivot.right = null;
-        this.leftLeftRotation(changeNode)
+        leftLeftRotation(root)
 
 }
+
+ let node = new TreeNode(20);
+    let node15 = new TreeNode(15);
+    let node25 = new TreeNode(25);
+    let node10 = new TreeNode(10);
+    let node14 = new TreeNode(14);
+    node15.parent = node;
+    node25.parent = node;
+    node10.parent = node15
+    node14.parent = node10;
+
+    node.left = node15;
+    node.right = node25;
+    node.left.left = node10;
+    node.left.left.right = node14;
+
+    node.left.side = 'left';
+    node.right.side = 'right';
+    node.left.left.side = 'left';
+    node.left.left.right.side = 'left';
+
+    console.log(leftRightRotation(node10))
+
+    console.log(node)
 
 module.exports = {
     leftLeftRotation,
