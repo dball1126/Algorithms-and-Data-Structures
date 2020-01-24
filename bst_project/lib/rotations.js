584cc7fd405c6b64fd1ed3e976b43c0a01601bb9
daniel <g.thirteen333@gmail.com>
2020-01-24 16:19:21 -0500

    rightLeftRotation function complete for avl tree in js

--- bst_project/lib/rotations.js
+++ bst_project/lib/rotations.js
@@ -53,10 +53,51 @@ function leftRightRotation(node) {
 
 }
 
+function rightLeftRotation(node) {
+    if (!node) return null;
+    
+    let pivot = node;
+    let root = node.parent;
+    let changeNode = pivot.left;
+        root.right = changeNode;
+        
+        changeNode.parent = root;
+        changeNode.right = pivot;
+        changeNode.side = 'right';
+
+        pivot.parent = changeNode;
+        pivot.left = null;
+        rightRightRotation(root) // We pass in the root here.
+
+}
+let node = new TreeNode(20);
+    let node15 = new TreeNode(15);
+    let node25 = new TreeNode(25);
+    let node30 = new TreeNode(30);
+    let node27 = new TreeNode(27);
+    node15.parent = node;
+    node25.parent = node;
+    node30.parent = node25
+    node27.parent = node30;
+
+    node.right = node25;
+    node.left = node15;
+    node.right.right = node30;
+    node.right.right.left = node27;
+
+    node.left.side = 'left';
+    node.right.side = 'right';
+    node.right.right.side = 'right';
+    node.right.right.left.side = 'left';
+
+
+    rightLeftRotation(node30)
+    console.log(node)
 
 
 module.exports = {
     leftLeftRotation,
     rightRightRotation,
-    leftRightRotation
+    leftRightRotation,
+    rightLeftRotation
 }
